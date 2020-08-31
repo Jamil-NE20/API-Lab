af6d8bdccaf17701e9148e478ac4214327835e97
Md Jamil Uddin <azad1031@gmail.com>
2020-08-31 14:22:37 +0200

    Create controller for the API

--- WebAPI/2.CreateWebAPIScratch/Program.cs
+++ WebAPI/2.CreateWebAPIScratch/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace WebAPI
+namespace _2.CreateWebAPIScratch
 {
     public class Program
     {
