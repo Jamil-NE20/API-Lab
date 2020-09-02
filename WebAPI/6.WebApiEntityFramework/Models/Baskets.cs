fc657bb91e90e89d5b7bd29cf7ef424e0d7c42e6
Md Jamil Uddin <azad1031@gmail.com>
2020-09-02 16:35:14 +0200

    Working with Lab 6

---
+++ WebAPI/6.WebApiEntityFramework/Models/Baskets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.WebApiEntityFramework.Models
+{
+    public partial class Baskets
+    {
+        public Baskets()
+        {
+            BasketItems = new HashSet<BasketItems>();
+        }
+
+        public string Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public virtual ICollection<BasketItems> BasketItems { get; set; }
+    }
+}
